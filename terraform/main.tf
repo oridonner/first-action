b509beef189ca3be6d48561f40cb2902a1eb5163
EC2 Default User <ec2-user@ip-172-31-34-80.eu-west-2.compute.internal>
2023-03-05 12:30:23 +0000

    set terraform cloud

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,20 @@
 terraform {
-     required_version = ">= 0.12.24"
-     backend "s3" {
-          bucket = s3-filetype-terraform-backend
-          key = terraform.tfstate
-          region = eu-west-2
-     }
-}
-provider "aws" {
-     region = "eu-west-2"
-     access_key  = "AKIA2XFS2DQ2YVDECIEA"
-     secret_key = "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"
-}
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
 
+  backend "remote" {
+    organization = "oridonner"
+    workspaces {
+      name = "filetype"
+    }
+  }
+}
 # Creating Lambda IAM resource
 resource "aws_iam_role" "lambda_iam" {
   name = "filetype_lambda_function_role"
@@ -52,19 +55,19 @@ EOF
 
 # Creating Lambda function 
 module "lambda_function_container_image" {
-  source = "terraform-aws-modules/lambda/aws"
+  source        = "terraform-aws-modules/lambda/aws"
   attach_policy = true
   #policy = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-  policy = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
-  function_name   = var.lambda_function_name
-  create_package  = false
-  image_uri       = "736961895477.dkr.ecr.eu-west-2.amazonaws.com/lambda-function:${local.docker_image_tag}"
-  package_type    = "Image"
-  environment_variables = {      
-      email_password  = var.email_password
-      email_receiver  = var.email_receiver
-      email_sender    = var.email_sender
-      }
+  policy         = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+  function_name  = var.lambda_function_name
+  create_package = false
+  image_uri      = "736961895477.dkr.ecr.eu-west-2.amazonaws.com/lambda-function:${local.docker_image_tag}"
+  package_type   = "Image"
+  environment_variables = {
+    email_password = var.email_password
+    email_receiver = var.email_receiver
+    email_sender   = var.email_sender
+  }
 }
 
 
