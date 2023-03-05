68c81cfe0855494356402ddba3bbc057e9ece267
Ori Donner <oridonner@gmail.com>
2023-03-05 10:57:22 +0200

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,31 @@
+variable "email_password" {
+  default = ""
+}
+
+variable "email_receiver" {
+  default = ""
+}
+
+variable "email_sender" {
+  default = ""
+}
+
+variable "image_tag" {
+  default = ""
+}
+
+variable "lambda_function_name" {
+  default = ""
+}
+
+variable "s3_bucket_name" {
+  default = ""
+}
+
+variable "aws_access_key" {
+  default = ""
+}
+
+variable "aws_secret_key" {
+  default = ""
+}
